e8952ca153dca7af2e02ad94dea693c0742349a9
azaank817 <azaank817@gmail.com>
2025-05-09 01:16:50 -0700

    Update lru_replacement.cpp

--- lru_replacement.cpp
+++ lru_replacement.cpp
@@ -1,9 +1,7 @@
 /**
- * Assignment 5: Page replacement algorithms
+ * LRU Replacement Algorithm Implementation
  * @file lru_replacement.cpp
  * @author Azaan Khan
- * @brief A class implementing the LRU (Least Recently Used) page replacement algorithm
- * @version 1.0
  */
 
 #include "lru_replacement.h"
@@ -15,56 +13,47 @@ using namespace std;
 // Constructor
 LRUReplacement::LRUReplacement(int num_pages, int num_frames)
     : Replacement(num_pages, num_frames), current_time(0) {
-    // Initialize all page timestamps to 0
     last_used_time.resize(num_pages, 0);
 }
 
 // Destructor
-LRUReplacement::~LRUReplacement() {
-    // Nothing special to free
-}
+LRUReplacement::~LRUReplacement() {}
 
-// Called when an already-loaded page is accessed (used to track recency)
+// Update access time when a page is touched
 void LRUReplacement::touch_page(int page_num) {
     current_time++;
     last_used_time[page_num] = current_time;
 }
 
-// Called when an invalid page is accessed and free frames are available
+// Load a new page into a free frame
 void LRUReplacement::load_page(int page_num) {
     int frame_num = get_next_free_frame();
+    page_table.set_entry(page_num, frame_num, true);
 
-    // Mark page as valid and assign frame
-    page_table->set_entry(page_num, frame_num, true);
-
-    // Track access time
     current_time++;
     last_used_time[page_num] = current_time;
 
     num_page_faults++;
 }
 
-// Called when an invalid page is accessed and memory is full
+// Replace the least recently used page
 int LRUReplacement::replace_page(int page_num) {
-    int oldest_time = std::numeric_limits<int>::max();
+    int oldest_time = numeric_limits<int>::max();
     int lru_page = -1;
 
-    // Find the least recently used valid page
-    for (int i = 0; i < page_table->get_num_pages(); ++i) {
-        if (page_table->is_valid(i) && last_used_time[i] < oldest_time) {
+    // Find least recently used valid page
+    for (int i = 0; i < page_table.get_num_pages(); ++i) {
+        if (page_table.is_valid(i) && last_used_time[i] < oldest_time) {
             oldest_time = last_used_time[i];
             lru_page = i;
         }
     }
 
-    // Evict the LRU page
-    int frame_num = page_table->get_frame_number(lru_page);
-    page_table->set_entry(lru_page, -1, false);
+    // Evict LRU page
+    int frame_num = page_table.get_frame_number(lru_page);
+    page_table.set_entry(lru_page, -1, false);
+    page_table.set_entry(page_num, frame_num, true);
 
-    // Load the new page
-    page_table->set_entry(page_num, frame_num, true);
-
-    // Update access time for the new page
     current_time++;
     last_used_time[page_num] = current_time;
 
@@ -73,3 +62,4 @@ int LRUReplacement::replace_page(int page_num) {
 
     return frame_num;
 }
+
